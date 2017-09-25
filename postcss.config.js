99ac2f0b3e3cb84132553135a28648b771bd16ee
Will King <contact@wking.io>
2017-09-25 13:39:20 -0500

    removing untracked files

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ options, env }) => ({
-  parser: 'postcss-scss',
-  plugins: {
-    autoprefixer: env === 'production' ? options.autoprefixer : false,
-    cssnano: env === 'production' ? options.cssnano : false,
-  },
-});
