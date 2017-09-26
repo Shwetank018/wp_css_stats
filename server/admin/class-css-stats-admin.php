0d1680c390df5f6b07044f0c37372c325eb715b8
Will King <contact@wking.io>
2017-09-26 10:02:46 -0500

    Completed functionality ready for beta

--- server/admin/class-css-stats-admin.php
+++ server/admin/class-css-stats-admin.php
@@ -98,7 +98,7 @@ class Css_Stats_Admin {
 		wp_enqueue_script( $this->plugin_name . '-common', PLUGIN_DIR . 'client/dist/commons.js', array(), $this->version, true );
 		wp_enqueue_script( $this->plugin_name, PLUGIN_DIR . 'client/dist/admin.bundle.js', array(), $this->version, true );
 
-		wp_localize_script( $this->plugin_name, 'css_stats', $this->get_vars);
+		wp_localize_script( $this->plugin_name, 'css_stats', $this->get_vars());
 				
 	}
 
@@ -135,19 +135,23 @@ class Css_Stats_Admin {
 			exit("No naughty business please");
 		}
 		$filepath = $_POST['filepath'];
-		update_option('css_stats', $filepath);
+		update_option('css_stats_filepath', $filepath);
 		$data = array();
-		wp_send_json_success(['message' => 'we here']);
+		$files = glob(get_stylesheet_directory() . $filepath);
+    foreach ($files as $file) {
+			 array_push($data, str_replace(get_stylesheet_directory(), get_stylesheet_directory_uri(), $file));
+    }
+		wp_send_json_success(['files' => $data]);
 	  die();
 	}
 
-	public function get_vars($filepath) {
+	public function get_vars($filepath = false) {
 		if (!$filepath) { $filepath = get_option('css_stats_filepath'); }
     $css_stats = array();
     $css_stats['data']['files'] = array();
     $files = glob(get_stylesheet_directory() . $filepath);
     foreach ($files as $file) {
-      $css_stats['data']['files'] = array_push(str_replace(get_stylesheet_directory(), get_stylesheet_directory_uri(), $file));
+      array_push($css_stats['data']['files'], str_replace(get_stylesheet_directory(), get_stylesheet_directory_uri(), $file));
     }
     $css_stats['data']['filepath'] = $filepath;
     $css_stats['ajaxurl'] = admin_url( 'admin-ajax.php' );
